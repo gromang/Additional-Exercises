12d5dc6a534ba2dfc6f45f9f4af0fb35b03f37c3
Moiseev Roman <lp15.mr@yandex.ru>
2019-12-01 22:21:00 +0300

    2 of 5

---
+++ for_dict_challenges.py
@@ -0,0 +1,106 @@
+# Задание 1
+# Дан список учеников, нужно посчитать количество повторений каждого имени ученика.
+students = [
+    {'first_name': 'Вася'},
+    {'first_name': 'Петя'},
+    {'first_name': 'Маша'},
+    {'first_name': 'Маша'},
+    {'first_name': 'Петя'},
+]
+stud_list = []
+for line in students:
+    stud_list.append(line['first_name'])
+for name in set(stud_list):
+    print(f'{name} : {stud_list.count(name)}')
+
+
+# Пример вывода:
+# Вася: 1
+# Маша: 2
+# Петя: 2
+
+
+# Задание 2
+# Дан список учеников, нужно вывести самое часто повторящееся имя.
+students = [
+    {'first_name': 'Вася'},
+    {'first_name': 'Петя'},
+    {'first_name': 'Маша'},
+    {'first_name': 'Маша'},
+    {'first_name': 'Оля'},
+]
+stud_list = []
+for line in students:
+    stud_list.append(line['first_name'])
+max_stud = 0
+for name in set(stud_list):
+    if stud_list.count(name) > max_stud:
+        max_stud = stud_list.count(name)
+        stud_name = name
+
+print(f'Самое частое имя среди учеников: {stud_name}')
+
+
+# Пример вывода:
+# Самое частое имя среди учеников: Маша
+
+# Задание 3
+# Есть список учеников в нескольких классах, нужно вывести самое частое имя в каждом классе.
+school_students = [
+    [  # это – первый класс
+        {'first_name': 'Вася'},
+        {'first_name': 'Вася'},
+    ],
+    [  # это – второй класс
+        {'first_name': 'Маша'},
+        {'first_name': 'Маша'},
+        {'first_name': 'Оля'},
+    ]
+]
+# ???
+
+# Пример вывода:
+# Самое частое имя в классе 1: Вася
+# Самое частое имя в классе 2: Маша
+
+
+# Задание 4
+# Для каждого класса нужно вывести количество девочек и мальчиков в нём.
+school = [
+    {'class': '2a', 'students': [
+        {'first_name': 'Маша'}, {'first_name': 'Оля'}]},
+    {'class': '3c', 'students': [
+        {'first_name': 'Олег'}, {'first_name': 'Миша'}]},
+]
+is_male = {
+    'Маша': False,
+    'Оля': False,
+    'Олег': True,
+    'Миша': True,
+}
+# ???
+
+# Пример вывода:
+# В классе 2a 2 девочки и 0 мальчика.
+# В классе 3c 0 девочки и 2 мальчика.
+
+
+# Задание 5
+# По информации о учениках разных классов нужно найти класс, в котором больше всего девочек и больше всего мальчиков.
+school = [
+    {'class': '2a', 'students': [
+        {'first_name': 'Маша'}, {'first_name': 'Оля'}]},
+    {'class': '3c', 'students': [
+        {'first_name': 'Олег'}, {'first_name': 'Миша'}]},
+]
+is_male = {
+    'Маша': False,
+    'Оля': False,
+    'Олег': True,
+    'Миша': True,
+}
+# ???
+
+# Пример вывода:
+# Больше всего мальчиков в классе 3c
+# Больше всего девочек в классе 2a
